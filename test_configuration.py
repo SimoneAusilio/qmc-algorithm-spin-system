64616b829e7352a009f0c666c95ae1a5144baade
Simone Ausilio <simone.ausilio@polytechnique.edu>
2022-11-15 18:34:25 +0100

    Test configuration snippet

---
+++ test_configuration.py
@@ -0,0 +1,15 @@
+import configuration
+import spin
+import Projet_Code
+
+'''Test for two and three spins'''
+L=4
+Beta=20
+m=1
+Jx=1
+Jz=2
+chess=Projet_Code.Chessboard(L, Beta, m, Jx, Jz)
+conf=configuration.Configuration(chess)
+print(conf.config)
+chess._config_to_image()
+#print(conf.get_energy(0))
