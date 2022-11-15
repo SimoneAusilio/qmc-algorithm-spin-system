af0e7e6c621c27e98d7eae48c211822648f62b88
Nunzia RC <nunzia.revel-carsalade@polytechnique.edu>
2022-11-15 18:00:09 +0100

    "adding local_update still need to debug it a bit

--- Projet_Code.py
+++ Projet_Code.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import collections  as mc
+from matplotlib import collections  as mc 
+from random import *
 # class Mother_Square:
 #     "Abstract Square saving the various weight"
 #     def __init__(self,Dtau,Jx,Jz):
@@ -69,6 +70,9 @@ class Chessboard:
         "give the weight of a given type, type in [1,2,3,4,5,6]"
         return self.weight_list[(sqr_type-1)//2]
 
+    def _get_config(self):
+        return self.worldlines_board
+
     def _config_to_image(self):
         "plot the chessboard with its current wordlines"
 
@@ -111,6 +115,61 @@ class Chessboard:
         ax.add_collection(lc)
 
         plt.show()
+    
+    def local_update(self):
+        "effectuate one step of the local_update algorithme"
+        "choose uniformly a black square among the available ones for an update"
+        "Use a metropolis rules to choose wether or not a change should happened"
+        config = self.worldlines_board
+        L = self.size
+        m = self.m
+
+        "reportering the possible black square for a local update"
+        possible_update = []
+        for j in range(0,L-2,2):
+            for i in range(0,m-2,2):
+                left_c = config[i][j]._get_square_type()
+                if left_c == 5 or left_c == 2:
+                    right_c = config [i][j+2]._get_square_type()
+                    if right_c == 2 or right_c == 6:
+                        possible_update.append([i,j+1,left_c,right_c])
+        print("phase 1")
+        "Draw one at random and see if it is accepted"
+        #left,right,up,down_c are the old configurations, Left, Right,Up,Down_c are the new"
+        random_draw = randint(0,len(possible_update)-1)
+        i,j,left_c,right_c = possible_update[random_draw]
+        up_c = config[i+1,j]._get_square_type()
+        down_c = config[i-1,j]._get_square_type()
+        print("phase 2")
+        # computing the new conf
+        if left_c == 5: Left_c = 1
+        else : Left_c = 6
+        if right_c == 2 : Right_c = 5
+        else : Right_c = 1
+        if up_c == 1 : Up_c = 3
+        else : Up_c = 2
+        if down_c == 1 : Down_c = 4
+        else : Down_c = 2
+        print(left_c,Left_c)
+        #computing DeltaE
+        W_i = 0
+        W_f = 0
+        for k in [left_c,right_c,up_c,down_c]:
+            W_i += self._get_weight(k)
+        for k in [Left_c,Right_c,Up_c,Down_c]:
+            W_f += self._get_weight(k)
+        Delta_W = W_f/W_i
+        print(Delta_W)
+        "Do the change if it was accepted"
+        #metropolis accepting protocol and changing conf
+        if Delta_W >= random():
+            print("phase 5")
+            config[i-1][j] = Square(Down_c,self)
+            config[i+1][j] = Square(Up_c,self)
+            config[i][j-1] = Square(Left_c,self)
+            config[i][j+1] = Square(Right_c,self)  
+
+        #return new energy?           
 
 
 
@@ -124,6 +183,9 @@ class Square(Chessboard):
         "update the attribute of the square"
         self.square_type = sqr_type
         self.weight = Chessboard._get_weight(sqr_type)
+    
+    def _get_square_type(self):
+        return self.square_type
 
 
 
