correction bug on the energy computation

configuration.py
@@ -9,48 +9,52 @@ class Configuration:
 
     def __init__(self, chess):
         '''Initialize spin configurations'''
-        self.size=chess.size+1
+        self.size=chess.size
         self.Jx=chess.Jx
         self.Jz=chess.Jz
         self.m=chess.m
         self.config=np.zeros((2*self.m, self.size), dtype=int)
-        # iteration over of rows from the bottom
+        self._update_configuration(chess)
+
+    def _update_configuration(self,chess):
+    # iteration over of rows from the bottom
         for i in range(2*self.m-1, -1, -1):
             # iteration over columns of the square
             for j in range(chess.size):
                 if (i+j)%2==1:  # positions of white squares
-                    n=chess.worldsquare_board[i][j]._get_square_type
+                    n=chess.worldsquare_board[i][j]._get_square_type()
                     # cases (same order from fig. 10.1 of the chapter)
                     if n==1:
                         self.config[i][j]=+1
-                        self.config[i][j+1]=-1
+                        self.config[i][(j+1)%(self.size)]=-1
                         self.config[(i+1)%(2*self.m)][j]=+1
-                        self.config[(i+1)%(2*self.m)][j+1]=-1
+                        self.config[(i+1)%(2*self.m)][(j+1)%(self.size)]=-1
                     elif n==2:
                         self.config[i][j]=-1
-                        self.config[i][j+1]=+1
+                        self.config[i][(j+1)%(self.size)]=+1
                         self.config[(i+1)%(2*self.m)][j]=-1
-                        self.config[(i+1)%(2*self.m)][j+1]=+1
+                        self.config[(i+1)%(2*self.m)][(j+1)%(self.size)]=+1
                     elif n==3:
                         self.config[i][j]=-1
-                        self.config[i][j+1]=+1
+                        self.config[i][(j+1)%(self.size)]=+1
                         self.config[(i+1)%(2*self.m)][j]=+1
-                        self.config[(i+1)%(2*self.m)][j+1]=-1
+                        self.config[(i+1)%(2*self.m)][(j+1)%(self.size)]=-1
                     elif n==4:
                         self.config[i][j]=+1
-                        self.config[i][j+1]=-1
+                        self.config[i][(j+1)%(self.size)]=-1
                         self.config[(i+1)%(2*self.m)][j]=-1
-                        self.config[(i+1)%(2*self.m)][j+1]=+1
+                        self.config[(i+1)%(2*self.m)][(j+1)%(self.size)]=+1
                     elif n==5:
                         self.config[i][j]=+1
-                        self.config[i][j+1]=+1
+                        self.config[i][(j+1)%(self.size)]=+1
                         self.config[(i+1)%(2*self.m)][j]=+1
-                        self.config[(i+1)%(2*self.m)][j+1]=+1
+                        self.config[(i+1)%(2*self.m)][(j+1)%(self.size)]=+1
                     elif n==6:
                         self.config[i][j]=-1
-                        self.config[i][j+1]=-1
+                        self.config[i][(j+1)%(self.size)]=-1
                         self.config[(i+1)%(2*self.m)][j]=-1
-                        self.config[(i+1)%(2*self.m)][j+1]=-1
+                        self.config[(i+1)%(2*self.m)][(j+1)%(self.size)]=-1
+    
 
     def _get_energy(self, i):
         '''Returns the enrgy of the configuration at row i'''

test_local_update.py
@@ -26,7 +26,7 @@ im=ax1.imshow(chess.binary_chessboard, cmap='binary_r', interpolation='nearest',
 color_red=(1, 0, 0, 1)
 lines=chess.worldlines_board
 lc=mc.LineCollection(lines, colors=color_red, linewidths=4)
-#ax1.add_collection(lc)
+ax1.add_collection(lc)
 # energy per spin
 ax2=fig.add_subplot(1, 2, 2)
 ax2.set_title("Energy per spin")
@@ -41,20 +41,17 @@ energ=[]
 
 def worldline_anim(n):
     '''Animation function for update algorithm'''
-    # keeping track of the old lines
-    old_lines = chess._get_worldlines_board()
-    old_lc=mc.LineCollection(old_lines, colors=color_red, linewidths=4)
-    old_lc.remove()
     # update trial
     for i in range(length_cycle):
         chess.local_update()
+    conf._update_configuration(chess)
     e=np.average(np.array([conf._get_energy(i) for i in range(2*conf.m)]))/conf.size
     # plot update (left)
     current_lines=chess._get_worldlines_board()
     current_lc=mc.LineCollection(current_lines, colors=color_red, linewidths=4)
-    ax1.add_collection(current_lc)
+    ax1.collections[0].set_segments(current_lines)
     
-    #current_lc.remove()
+    
     # plot update (right)
     if len(time)<N: time.append(n)
     if len(energ)<N: energ.append(e)
@@ -64,7 +61,7 @@ def worldline_anim(n):
     curve.set_data(time, energ)
     return (lines, curve)
 
-animation=FuncAnimation(fig, worldline_anim, interval=1, blit=False)
+animation=FuncAnimation(fig, worldline_anim, interval=100, blit=False)
 plt.show()
 '''
 # data collection